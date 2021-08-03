d205afd9130623b69bdfe2f4901b4496b48e1e44
thkim7527 <thkim7527@gmail.com>
2021-08-03 22:59:07 +0900

    Add loginForm

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Hi");
+const loginInput = document.querySelector("#loginForm input");
+const loginButton = document.querySelector("#loginForm button");
+
+function onLoginButtonClick() {
+    const userName = loginInput.value;
+    console.log(userName);
+}
+
+loginButton.addEventListener("click", onLoginButtonClick);
