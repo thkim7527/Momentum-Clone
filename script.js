cd6570f1a2f42fd75c98eca374b7582a02383dd6
thkim7527 <thkim7527@gmail.com>
2021-08-04 12:05:28 +0900

    Add greeting message

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+const HIDDEN_CLASSNAME = "hidden";
+
 const loginForm = document.querySelector("#loginForm");
 const loginInput = document.querySelector("#loginForm input");
+
+const greeting = document.querySelector("#greeting");
 
 function handleLoginFormSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
-    console.log(userName);
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
+    greeting.innerText = `Hello, ${userName}.`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", handleLoginFormSubmit);
