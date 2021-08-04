936cf1f49f05a68c02218a459468ba8d001fbb3f
thkim7527 <thkim7527@gmail.com>
2021-08-04 12:52:33 +0900

    Add localStorage

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ const HIDDEN_CLASSNAME = "hidden";
 const loginForm = document.querySelector("#loginForm");
 const loginInput = document.querySelector("#loginForm input");
 
+const savedUserName = localStorage.getItem("userName");
+const USERNAME_KEY = "userName"
+
 const greeting = document.querySelector("#greeting");
+
+
+
+function showGreeting() {
+    greeting.innerText = `Hello, ${savedUserName}.`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function handleLoginFormSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     
-    greeting.innerText = `Hello, ${userName}.`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    showGreeting();
 }
 
-loginForm.addEventListener("submit", handleLoginFormSubmit);
+
+
+if(savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", handleLoginFormSubmit);
+} else {
+    showGreeting();
+}
